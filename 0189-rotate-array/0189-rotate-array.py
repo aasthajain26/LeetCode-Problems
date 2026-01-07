d390cf904253c3d50f57c42d5dfc63a7ac8f74b0
aasthajain26 <99245012+aasthajain26@users.noreply.github.com>
2026-01-07 12:46:29 +0530

    Time: 8 ms (28.33%), Space: 27.1 MB (6.11%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,8 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        while(k >0):
-            a=nums.pop()
-            nums.insert(0,a)
-            k -=1
-        return nums
+        def rotate_array_right(arr,start,end):
+            while (start<end):    
+                arr[start],arr[end]= arr[end],arr[start]
+                start +=1
+                end -=1
+                
+        n= len(nums)
+        k= k%n
+
+        #reverse whole array
+        rotate_array_right(nums,0,n-1)
+        #reverse k elements from the start
+        rotate_array_right(nums,0,k-1)
+        #reverse all elements from the kth element
+        rotate_array_right(nums,k,n-1)
+
+
+
+
+
+
