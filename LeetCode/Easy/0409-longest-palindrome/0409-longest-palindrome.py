96fa5f00f84049bd50f62e3c5b4ae99e4f43f8b0
aasthajain26 <99245012+aasthajain26@users.noreply.github.com>
2026-01-14 13:30:52 +0530

    Time: 2 ms (51.57%), Space: 19.2 MB (12.28%) - LeetHub

---
+++ LeetCode/Easy/0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        dict1= {}
+
+        for i in s:
+            if i in dict1:
+                dict1[i] +=1
+            else:
+                dict1[i] =1
+
+        cnt = 0 
+        for i in dict1:
+            if dict1[i] %2 ==0:
+                cnt +=dict1[i]
+            elif dict1[i] ==1:
+                continue
+            else:
+                cnt += int((dict1[i]-1))
+                dict1[i] =1
+            
+        for i in dict1:
+            if dict1[i]==1:
+                cnt += 1
+                break
+
+        return cnt
