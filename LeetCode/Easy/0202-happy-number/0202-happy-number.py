414159421cebe4652ef83785a40f275b50501b5f
aasthajain26 <99245012+aasthajain26@users.noreply.github.com>
2026-01-24 14:20:30 +0530

    Time: 0 ms (100%), Space: 19.3 MB (20.21%) - LeetHub

--- LeetCode/Easy/0202-happy-number/0202-happy-number.py
+++ LeetCode/Easy/0202-happy-number/0202-happy-number.py
@@ -3,21 +3,19 @@ class Solution:
         if n <=0:
             return False
         l = set()
-        s =0
-        while(n!=1):
 
+        while(n!=1):
+            if n in l:
+                return False
             l.add(n)
+            s=0
             while(n!=0):
 
                 a= n%10
                 s += a*a 
                 n = n//10
+            n = s
 
-            if s in l:
-                return False
-
-            n =s
-            s=0
             
 
         return True
